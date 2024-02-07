3365961bb4ff7d90512013fe05abc4f46ae848b3
Abnerlr <abnerlr89@gmail.com>
2024-02-07 13:52:38 -0600

    Busqueda de un producto con funcion filter

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,5 +1,5 @@
 from uuid import uuid4 as uuid
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 class Producto(BaseModel):
@@ -31,8 +31,14 @@ def crear_producto(producto: Producto):
 
 @app.get('/producto/{producto_id}')
 def obtener_producto_por_id(producto_id: str):
-    for p in productos:
-        if p.id == producto_id:
-            return p
+#    for p in productos:
+#        if p.id == producto_id:
+#            return p
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+
+    if len(resultado):
+        return resultado[0]
         
-    return{'mensaje': f'El producto con el id {producto_id} no fue encontrado'}
+    #return{'mensaje': f'El producto con el id {producto_id} no fue encontrado'}
+
+    raise HTTPException(status_code=404, detail=f'El producto con id {producto_id} no fue encontrado')
