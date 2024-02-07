65c9a73218741c427e3a8e63c9915b2e344705e3
Abnerlr <abnerlr89@gmail.com>
2024-02-07 10:58:55 -0600

    Funcion para crear un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Producto(BaseModel):
-    id: Optional[str]
+    id: Optional[str] = None
     nombre: str
     precio_compra: float
     precio_venta: float
@@ -20,4 +20,10 @@ def index():
 @app.get('/producto')
 def obtener_productos():
     return productos
+
+
+@app.post('/producto')
+def crear_producto(producto: Producto):
+    productos.append(producto)
+    return {'message': 'Producto creado satisfactoriamente'}
 
